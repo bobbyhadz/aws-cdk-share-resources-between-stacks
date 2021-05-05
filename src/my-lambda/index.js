bc826bfa188ed0e7d8b9f8240303e3f849ed518f
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-05 17:16:43 +0300

    updated function code

--- src/my-lambda/index.js
+++ src/my-lambda/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/require-await */
 
 async function main(event) {
   console.log('BUCKET_NAME 👉', process.env.BUCKET_NAME);
 
   return {
-    body: JSON.stringify({message: 'SUCCESS 🎉'}),
+    body: JSON.stringify({message: `${process.env.BUCKET_NAME} 🎉`}),
     statusCode: 200,
   };
 }
